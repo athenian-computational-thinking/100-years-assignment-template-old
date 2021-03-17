2d96b5e2d8a3df62f52ebd57e23df1b0b1b6de7b
Megan Leich <51799170+maleich@users.noreply.github.com>
2021-03-17 09:04:00 -0700

    Update my_test.py

--- my_test.py
+++ my_test.py
@@ -2,6 +2,6 @@ from my_code import year_100
 
 
 def test_inc():
-    assert 2105 == year_100(15)
-    assert 2120 == year_100(0)
-    assert 2095 == year_100(25)
+    assert 2106 == year_100(15)
+    assert 2121 == year_100(0)
+    assert 2096 == year_100(25)
